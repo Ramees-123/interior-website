382711845256022e20d0248e90ab5e3d3bf9915d
Ramees-123 <chukkanrameesramees@gmail.com>
2026-03-29 00:09:29 +0530

    update

--- src/app/components/portfolio/portfolio.ts
+++ src/app/components/portfolio/portfolio.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, AfterViewInit, OnDestroy, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.css',
 })
